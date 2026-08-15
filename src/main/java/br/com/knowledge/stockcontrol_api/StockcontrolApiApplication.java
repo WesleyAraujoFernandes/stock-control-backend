064d72007ca9d66963f0fbb825514aa90d2142c5
WesleyAraujoFernandes <wesley@viaapia.com.br>
2026-08-15 15:00:22 -0300

    projeto inicial validado

---
+++ src/main/java/br/com/knowledge/stockcontrol_api/StockcontrolApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.knowledge.stockcontrol_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockcontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockcontrolApiApplication.class, args);
+	}
+
+}
